3170cd5c9d8057b446186094cdfa723c9935a8b5
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-12 17:20:43 -0300

    Add server.js with FCC settings

---
+++ metricImperialConverter_project/server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+require('dotenv').config();
+
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+let app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+fccTestingRoutes(app);
+
+apiRoutes(app);  
+    
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log("Listening on port " + port);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+          console.log('Tests are not valid:');
+          console.error(e);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app; //for testing
