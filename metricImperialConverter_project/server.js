9160be1205eb532929ad8c0ca9da55885e9775d5
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-13 13:49:16 -0300

    Format document with Slint

--- metricImperialConverter_project/server.js
+++ metricImperialConverter_project/server.js
@@ -1,54 +1,49 @@
-'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const { expect } = require('chai');
+const createCorsMiddleware = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api');
+const fccTestingRoutes = require('./routes/fcctesting');
+const runner = require('./test-runner');
 
-let app = express();
+const app = express();
+const urlencodedOpts = { extended: true };
+const corsMiddlewareOpts = { origin: '*' };
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(createCorsMiddleware(corsMiddlewareOpts));
 
-app.use(cors({origin: '*'}));
+app.use(express.urlencoded(urlencodedOpts));
+app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route('/').get((req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
 
 fccTestingRoutes(app);
 
-apiRoutes(app);  
-    
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+apiRoutes(app);
+
+app.use((req, res, next) => {
+  res.status(404).type('text').send('Not Found');
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  console.log("Listening on port " + port);
-  if(process.env.NODE_ENV==='test') {
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(() => {
       try {
         runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
+      } catch (e) {
+        console.log('Tests are not valid:');
+        console.error(e);
       }
     }, 1500);
   }
 });
 
-module.exports = app; //for testing
+module.exports = app; // for testing
