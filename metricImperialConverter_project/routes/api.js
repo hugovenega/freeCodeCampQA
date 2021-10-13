ceaaa570cea7e9615de23cb2dcee56b8552f22d6
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-13 14:38:11 -0300

    Add Route / API / Convert

--- metricImperialConverter_project/routes/api.js
+++ metricImperialConverter_project/routes/api.js
@@ -1,10 +1,21 @@
-'use strict';
-
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const { expect } = require('chai');
+const ConvertHandler = require('../controllers/convertHandler');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+  const convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+    const { input } = req.query;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+    );
+  });
 };
