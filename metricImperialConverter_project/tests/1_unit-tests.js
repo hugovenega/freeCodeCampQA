3626ca9ba6c188308e0ed00a84dcc1f8b07477b0
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-13 15:08:27 -0300

    Add unit tests

--- metricImperialConverter_project/tests/1_unit-tests.js
+++ metricImperialConverter_project/tests/1_unit-tests.js
@@ -1,9 +1,48 @@
 const chai = require('chai');
-let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const mocha = require('mocha');
 
-let convertHandler = new ConvertHandler();
+const { assert } = chai;
+const ConvertHandler = require('../controllers/convertHandler');
 
-suite('Unit Tests', function(){
+const convertHandler = new ConvertHandler();
 
+mocha.suite('Unit Tests', () => {
+  mocha.suite('ConvertHandler functions test', () => {
+    mocha.test('Whole number input', (done) => {
+      const input = '5L';
+      assert.equal(convertHandler.getNum(input), 5);
+      done();
+    });
+
+    mocha.test('Invalid input (double fraction)', (done) => {
+      const input = '4/3.1/7kg';
+      assert.equal(convertHandler.getNum(input), undefined);
+      done();
+    });
+
+    mocha.test('Decimal Input', (done) => {
+      const input = '5.2gal';
+      assert.equal(convertHandler.getNum(input), 5.2);
+      done();
+    });
+
+    mocha.test('Fractional Input', (done) => {
+      const input = '7/5mi';
+      assert.equal(convertHandler.getNum(input), 7 / 5);
+      done();
+    });
+
+    mocha.test('Fractional input with decimal', (done) => {
+      const input = '7/1.4mi';
+      assert.equal(convertHandler.getNum(input), 7 / 1.4);
+      done();
+    });
+
+    mocha.test('No numeric input', (done) => {
+      const input = 'lbs';
+      assert.equal(convertHandler.getNum(input), 1);
+      assert.equal(convertHandler.getUnit(input), 'lbs');
+      done();
+    });
+  });
 });
