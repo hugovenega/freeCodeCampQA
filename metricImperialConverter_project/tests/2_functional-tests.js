3b4c2deacabea41d9a9bdc90c446baa082339ab2
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-13 15:24:18 -0300

    Add mocha/chai test in api/convert route

--- metricImperialConverter_project/tests/2_functional-tests.js
+++ metricImperialConverter_project/tests/2_functional-tests.js
@@ -1,10 +1,26 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
-let assert = chai.assert;
+const mocha = require('mocha');
 const server = require('../server');
 
+const { assert } = chai;
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+mocha.suite('Functional Tests', () => {
+  mocha.suite('GET /api/convert => conversion object', () => {
+    mocha.test('Convert 4L (valid input)', (done) => {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: '4L' })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 4);
+          assert.equal(res.body.initUnit, 'L');
+          assert.approximately(res.body.returnNum, 1.056688, 0.1);
+          assert.equal(res.body.returnUnit, 'gal');
+          done();
+        });
+    });
+  });
 });
