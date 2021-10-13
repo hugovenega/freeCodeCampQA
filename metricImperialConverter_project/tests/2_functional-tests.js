947c9827272862bca151b5aacc11c96e91fa0ef3
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-13 15:26:01 -0300

    Add more mocha/chai test in api/convert route

--- metricImperialConverter_project/tests/2_functional-tests.js
+++ metricImperialConverter_project/tests/2_functional-tests.js
@@ -23,4 +23,50 @@ mocha.suite('Functional Tests', () => {
         });
     });
   });
+
+  mocha.test('Convert 5g (invalid input unit)', (done) => {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: '5g' })
+      .end((err, res) => {
+        assert.equal(res.body.initUnit, undefined);
+        done();
+      });
+  });
+
+  mocha.test('Convert 4/3.2/7L (invalid number)', (done) => {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: '4/3.2/7L' })
+      .end((err, res) => {
+        assert.equal(res.body.initNum, undefined);
+        done();
+      });
+  });
+
+  mocha.test('Convert 4/3.2/7g (invalid number and unit)', (done) => {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: '4/3.2/7g' })
+      .end((err, res) => {
+        assert.equal(res.body.initNum, undefined);
+        assert.equal(res.body.initUnit, undefined);
+        done();
+      });
+  });
+
+  mocha.test('Convert kg (no number)', (done) => {
+    chai
+      .request(server)
+      .get('/api/convert')
+      .query({ input: 'kg' })
+      .end((err, res) => {
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, 'kg');
+        done();
+      });
+  });
 });
