6f7262649156b0b84776d2e103648aaedad2c58d
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-14 13:15:51 -0300

    Remove unnecessary files

--- issueTracker_project/controllers/helper.controller.js
+++
@@ -1,9 +0,0 @@
-const Helper = {
-  removeUndefinedAndEmptyStringValuesFromObj: (obj) => {
-    Object.keys(obj).forEach((k) => !obj[k] && obj[k] !== false && delete obj[k]);
-    return obj;
-  },
-  convertStringToBoolean: (string) => (string.toLowerCase() === 'true'),
-  checkIsEmptyObject: (obj) => Object.keys(obj).length === 0 && obj.constructor === Object,
-};
-module.exports = Helper;
--- issueTracker_project/controllers/issue.controller.js
+++
@@ -1,3 +0,0 @@
-const Issue = require('../models/issues');
-const Project = require('../models/projects');
-const Helper = require('./helper.controller');
