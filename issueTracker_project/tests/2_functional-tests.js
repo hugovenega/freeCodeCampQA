8ee1b4bc93fe48234e51818f69181bceb40076e5
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-14 16:44:51 -0300

    Improve the tests by entering .post in the POST tests suite

--- issueTracker_project/tests/2_functional-tests.js
+++ issueTracker_project/tests/2_functional-tests.js
@@ -53,58 +53,57 @@ mocha.suite('Functional Tests', () => {
           done();
         });
     });
-  });
+    // Create an issue with only required fields: POST request to /api/issues/{project}
 
-  // Create an issue with only required fields: POST request to /api/issues/{project}
+    mocha.test('Create an Issue With Only Required Fields', (done) => {
+      const testData = {
+        issue_title: 'Test Title',
+        issue_text: 'Test Text',
+        created_by: 'Text- Create an Issue With Every Field Filled In',
+      };
+      chai.request(server)
+        .post('/api/issues/test')
+        .send(testData)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.nestedInclude(res.body, testData);
+          assert.property(res.body, 'created_on');
+          assert.isNumber(Date.parse(res.body.created_on));
+          assert.property(res.body, 'updated_on');
+          assert.isNumber(Date.parse(res.body.updated_on));
+          assert.property(res.body, 'open');
+          assert.isBoolean(res.body.open);
+          assert.isTrue(res.body.open);
+          assert.property(res.body, '_id');
+          assert.isNotEmpty(res.body._id);
+          assert.property(res.body, 'issue_title');
+          assert.equal(res.body.issue_title, 'Test Title');
+          assert.property(res.body, 'issue_text');
+          assert.equal(res.body.issue_text, 'Test Text');
+          assert.property(res.body, 'issue_text');
+          assert.equal(res.body.issue_text, 'Test Text');
+          assert.property(res.body, 'created_by');
+          assert.equal(res.body.created_by, 'Text- Create an Issue With Every Field Filled In');
+          done();
+        });
+    });
 
-  mocha.test('Create an Issue With Only Required Fields', (done) => {
-    const testData = {
-      issue_title: 'Test Title',
-      issue_text: 'Test Text',
-      created_by: 'Text- Create an Issue With Every Field Filled In',
-    };
-    chai.request(server)
-      .post('/api/issues/test')
-      .send(testData)
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body);
-        assert.nestedInclude(res.body, testData);
-        assert.property(res.body, 'created_on');
-        assert.isNumber(Date.parse(res.body.created_on));
-        assert.property(res.body, 'updated_on');
-        assert.isNumber(Date.parse(res.body.updated_on));
-        assert.property(res.body, 'open');
-        assert.isBoolean(res.body.open);
-        assert.isTrue(res.body.open);
-        assert.property(res.body, '_id');
-        assert.isNotEmpty(res.body._id);
-        assert.property(res.body, 'issue_title');
-        assert.equal(res.body.issue_title, 'Test Title');
-        assert.property(res.body, 'issue_text');
-        assert.equal(res.body.issue_text, 'Test Text');
-        assert.property(res.body, 'issue_text');
-        assert.equal(res.body.issue_text, 'Test Text');
-        assert.property(res.body, 'created_by');
-        assert.equal(res.body.created_by, 'Text- Create an Issue With Every Field Filled In');
-        done();
-      });
-  });
+    // Create an issue with missing required fields: POST request to /api/issues/{project}
 
-  // Create an issue with missing required fields: POST request to /api/issues/{project}
-
-  mocha.test('Create an Issue With Missing Required Fields', (done) => {
-    const testData = {
-      issue_title: 'Test Title',
-      issue_text: 'Test Text',
-    };
-    chai.request(server)
-      .post('/api/issues/test')
-      .send(testData)
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.equal(res.body.error, 'required field(s) missing');
-        done();
-      });
+    mocha.test('Create an Issue With Missing Required Fields', (done) => {
+      const testData = {
+        issue_title: 'Test Title',
+        issue_text: 'Test Text',
+      };
+      chai.request(server)
+        .post('/api/issues/test')
+        .send(testData)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'required field(s) missing');
+          done();
+        });
+    });
   });
 });
