1d218e53eac0c72b93c07cf706718de1fc381ee9
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-14 15:08:39 -0300

    Add post controller

--- issueTracker_project/routes/api.js
+++ issueTracker_project/routes/api.js
@@ -22,7 +22,7 @@ module.exports = function (app) {
       if (err) console.log(err);
       let filteredIssue = populatedProject[0].issue;
       if (searchQuery) {
-        for (let parameter in searchQuery) {
+        for (const parameter in searchQuery) {
           filteredIssue = filteredIssue.filter((issue) => issue[parameter] == searchQuery[parameter]);
         }
       }
@@ -32,6 +32,66 @@ module.exports = function (app) {
 
     .post((req, res) => {
       const { project } = req.params;
+      const { body } = req;
+
+      if (!body.issue_title || !body.issue_text || !body.created_by) {
+        res.json({
+          error: 'required field(s) missing',
+        });
+      } else {
+      // look if project already exist
+        Project.findOne({
+          name: project,
+        }, (foundProjectErr, foundProject) => {
+          if (foundProjectErr) {
+            console.log(foundProjectErr);
+          // if there is no project
+          } else if (foundProject === null) {
+            Project.create({
+              name: project,
+            }, (createProjectErr, newProject) => {
+              if (createProjectErr) {
+                console.log(createProjectErr);
+              } else {
+                Issue.create({
+                  issue_title: body.issue_title,
+                  issue_text: body.issue_text,
+                  created_by: body.created_by,
+                  assigned_to: body.assigned_to,
+                  status_text: body.status_text,
+                }, (createIssueErr, newIssue) => {
+                  if (createIssueErr) {
+                    console.log(createIssueErr);
+                  }
+                  // made new project and new issue now push new issue to newly made project
+                  newProject.issue.push(newIssue);
+                  newProject.save((saveProjectErr, savedProject) => {
+                    saveProjectErr ? console.log(saveProjectErr) : res.json(newIssue);
+                  });
+                });
+              }
+            });
+          // if we get matched project
+          } else if (foundProject) {
+          // create a new issue
+            Issue.create({
+              issue_title: body.issue_title,
+              issue_text: body.issue_text,
+              created_by: body.created_by,
+              assigned_to: body.assigned_to,
+              status_text: body.status_text,
+            }, (err, newIssue) => {
+              if (err) {
+                console.log(err);
+              }
+              foundProject.issue.push(newIssue);
+              foundProject.save((err, savedProject) => {
+                err ? console.log(err) : res.json(newIssue);
+              });
+            });
+          }
+        });
+      }
     })
 
     .put((req, res) => {
