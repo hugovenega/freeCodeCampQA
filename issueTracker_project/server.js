a0b2183fe018994699c46b4bdf36079b2d534653
Hugo Venega <hugo.venega.00@gmail.com>
2021-10-14 16:27:15 -0300

    Add all requires

--- issueTracker_project/server.js
+++ issueTracker_project/server.js
@@ -4,30 +4,16 @@ const { expect } = require('chai');
 const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes = require('./routes/api.js');
-const fccTestingRoutes = require('./routes/fcctesting.js');
+const mongoose = require('mongoose');
+const Issue = require('./models/issues');
+const Project = require('./models/projects');
+
+const apiRoutes = require('./routes/api');
+const fccTestingRoutes = require('./routes/fcctesting');
 const runner = require('./test-runner');
 
 const app = express();
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.use(cors({ origin: '*' })); // For FCC testing purposes only
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Sample front-end
-app.route('/:project/')
-  .get((req, res) => {
-    res.sendFile(`${process.cwd()}/views/issue.html`);
-  });
-
-// Index page (static HTML)
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(`${process.cwd()}/views/index.html`);
-  });
 
 // For FCC testing purposes
 fccTestingRoutes(app);
@@ -43,16 +29,17 @@ app.use((req, res, next) => {
 });
 
 // Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
   if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(() => {
       try {
         runner.run();
       } catch (e) {
+        const error = e;
         console.log('Tests are not valid:');
-        console.error(e);
+        console.log(error);
       }
     }, 3500);
   }
